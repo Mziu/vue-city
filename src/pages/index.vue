ecee37aa3c384a25df7eb827a2bd093f111c6e1d
meta <16139991@qq.com>
2017-03-13 15:43:54 +0800

    edit

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
+    <div>{{ data.country + data.city + data.area}}</div>
     <select ref="sel1" @change="demo">
-      <option :value="cty.value" v-for="cty in city">{{cty.name}}</option>
+      <option :value="cty.name" v-for="cty in city">{{cty.name}}</option>
     </select>
-  <select ref="sel2" @change="demo2">
-      <option  :value="cty.value" v-for="cty in selection" >{{cty.name}}</option>
+    <select ref="sel2" @change="demo2">
+      <option  :value="cty.name" v-for="cty in selection" >{{cty.name}}</option>
     </select>
     <select>
-      <option :value="cty.value" v-for="cty in selection2">{{cty}}</option>
+      <option :value="cty.name" v-for="cty in selection2">{{cty}}</option>
     </select>
+    <div>{{selected}}</div>
   </div>
 </template>
 
@@ -23,13 +25,20 @@ export default {
     return {
       city: c,
       selected: 0,
-      selected2: 0
+      selected2: 0,
+      data: {
+        country: '上海',
+        city: '上海',
+        area: '普陀区',
+        street: '丹巴路1238号恩瓦德大厦1700层(吓死你?)'
+      }
     }
   },
   methods: {
     demo () {
       // 获取第一个select 选择的索引 返回给变量selected
       this.selected = this.$refs.sel1.selectedIndex
+      this.selected2 = 0 // 防止选择第一类没有第二类下标报错
     },
     demo2 () {
       // 获取第二个select 选择的索引 返回给变量selected2
@@ -52,7 +61,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 h1, h2 {
   font-weight: normal;
 }
